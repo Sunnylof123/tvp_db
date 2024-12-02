cc9d3d42ffc60a0331fe83bc868b1d4c3b1f0f99
sunnylof123 <93811722+Sunnylof123@users.noreply.github.com>
2024-12-02 18:35:15 +0800

    feat: add crm

---
+++ 2024/data/ver-2024-crm-data.sql
@@ -0,0 +1,4 @@
+-- Test
+
+INSERT INTO `users` VALUES ('1','admin','$2y$10$exlCdz7G/zgs/LIjtYHtNOLYF3eAqKZUph1TK0kGXcH8jPKcUSExG',0,NULL,NULL,1,NULL,'Administrator',1,0,1,NULL,'2024-08-26 07:42:08','2024-11-08 02:50:22','1','1','Administrator','favicon.png',NULL,NULL,NULL,NULL,NULL,NULL,'Active',NULL,NULL,NULL,NULL,NULL,0,0,1,'Active',NULL,NULL,'',0,0,NULL);
+
